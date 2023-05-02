ac55e4bf78083f16e817912a2106a8387310c64f
Harsh Dutta Tewari <103374169+Harsh-Tewari@users.noreply.github.com>
2023-05-03 02:09:18 +0530

    styled button

--- client/src/components/ReviewerDashboard/ReviewerDashboard.jsx
+++ client/src/components/ReviewerDashboard/ReviewerDashboard.jsx
@@ -1,42 +1,43 @@
 import { useEffect, React, useState } from "react";
 import { Link, json } from "react-router-dom";
-import {useToast} from '@chakra-ui/react';
+import { useToast } from "@chakra-ui/react";
 import "./ReviewerDashboard.css";
 import axios from "axios";
 
 const ReviewerDashboard = () => {
-
   const [biddingTender, setBiddingTender] = useState([]);
   const [allotedTender, setAllotedTender] = useState([]);
 
   const getTenders = async () => {
-    const res = await axios.get('/api/tender/tenderdisplay');
-    const getUser = await axios.get('/api/user/getUser');
-    let a = [], b = [];
+    const res = await axios.get("/api/tender/tenderdisplay");
+    const getUser = await axios.get("/api/user/getUser");
+    let a = [],
+      b = [];
     a = res.data.data;
     b = getUser.data.data;
-    localStorage.setItem('tenders', JSON.stringify(a));
-    localStorage.setItem('users', JSON.stringify(b));
+    localStorage.setItem("tenders", JSON.stringify(a));
+    localStorage.setItem("users", JSON.stringify(b));
 
     await getTenderStatus();
-  }
+  };
 
   const getTenderStatus = async () => {
-    const tenderData = JSON.parse(localStorage.getItem('tenders'));
-    const users = JSON.parse(localStorage.getItem('users'));
+    const tenderData = JSON.parse(localStorage.getItem("tenders"));
+    const users = JSON.parse(localStorage.getItem("users"));
 
     //allot and bit me reference number stored
-    let allot = [], bid = [];
+    let allot = [],
+      bid = [];
     for (let i = 0; i < tenderData.length; i++) {
-      if (tenderData[i].status === 'bidding') {
+      if (tenderData[i].status === "bidding") {
         bid.push(tenderData[i].referenceNumber);
-      }
-      else {
+      } else {
         allot.push(tenderData[i].referenceNumber);
       }
     }
 
-    let bidUser = [], allotUser = [];
+    let bidUser = [],
+      allotUser = [];
     for (let i = 0; i < users.length; i++) {
       for (let j = 0; j < users[i].biddedTenders.length; j++) {
         for (let k = 0; k < bid.length; k++) {
@@ -64,12 +65,12 @@ const ReviewerDashboard = () => {
   };
 
   useEffect(() => {
-    getTenders(); 
+    getTenders();
   }, []);
-  
+
   const toast = useToast();
 
-  const success = ()=>{
+  const success = () => {
     toast({
       title: "Rating Successfully Updated!",
       status: "success",
@@ -81,7 +82,6 @@ const ReviewerDashboard = () => {
 
   return (
     <div>
-
       <nav>
         <div className="navdet">
           <u>
@@ -119,28 +119,29 @@ const ReviewerDashboard = () => {
         <div className="tenderev" id="allotudb">
           Unreviewed Bids
         </div>
-        {
-          biddingTender.map((val)=>{
-            console.log(val);
-            return(
+        {biddingTender.map((val) => {
+          console.log(val);
+          return (
             <div className="revdata">
               <div className="textp1">
-                {
-                  val.biddedTenders.map((tid)=>{
-                    return(
-                      <div className="textspan">
-                        <h1 className="texth1">TenderID : &nbsp;</h1>
-                        <p className="textp1"> {tid}</p>
-                      </div>
-                    )
-                  })
-                }
+                {val.biddedTenders.map((tid) => {
+                  return (
+                    <div className="textspan">
+                      <h1 className="texth1">TenderID : &nbsp;</h1>
+                      <p className="textp1"> {tid}</p>
+                    </div>
+                  );
+                })}
                 <div className="textspan">
                   <h1 className="texth1">Bidder Name : &nbsp;</h1>
                   <p className="textp1">{val.name}</p>
                 </div>
                 <div className="textspan">
-                  <a href="http://localhost:4000/uploads/tender.docx" targer="blank" className="linktoprop">
+                  <a
+                    href="http://localhost:4000/uploads/tender.docx"
+                    targer="blank"
+                    className="linktoprop"
+                  >
                     Download Proposal
                   </a>
                 </div>
@@ -150,37 +151,39 @@ const ReviewerDashboard = () => {
                   <h1 className="texth1">Rating to bidder(Out of 10)</h1>
                   <input type="number" name="" className="ratingip" />
                 </div>
+                <button onClick={success} className="bsubsbh">
+                  Submit
+                </button>
               </div>
-              <button onClick={success}>Submit</button>
             </div>
-            );
-          })
-        }
+          );
+        })}
 
         <div className="tenderev" id="biddedudb">
           Previous Reviews
         </div>
-        {
-          allotedTender.map((val)=>{
-            return(
+        {allotedTender.map((val) => {
+          return (
             <div className="revdata">
               <div className="textp1">
-                {
-                  val.allotedTenders.map((tid)=>{
-                    return (
-                        <div className="textspan">
-                          <h1 className="texth1">TenderID : &nbsp;</h1>
-                          <p className="textp1"> {tid}</p>
-                        </div>
-                    )
-                  })
-                }
+                {val.allotedTenders.map((tid) => {
+                  return (
+                    <div className="textspan">
+                      <h1 className="texth1">TenderID : &nbsp;</h1>
+                      <p className="textp1"> {tid}</p>
+                    </div>
+                  );
+                })}
                 <div className="textspan">
                   <h1 className="texth1">Bidder Name : &nbsp;</h1>
                   <p className="textp1">{val.name}</p>
                 </div>
                 <div className="textspan">
-                  <a href="http://localhost:4000/uploads/tender.docx" targer="blank" className="linktoprop">
+                  <a
+                    href="http://localhost:4000/uploads/tender.docx"
+                    targer="blank"
+                    className="linktoprop"
+                  >
                     Download Proposal
                   </a>
                 </div>
@@ -190,12 +193,13 @@ const ReviewerDashboard = () => {
                   <h1 className="texth1">Rating to bidder(Out of 10)</h1>
                   <input type="number" name="" className="ratingip" />
                 </div>
+                <button onClick={success} className="bsubsbh">
+                  Submit
+                </button>
               </div>
-              <button onClick={success}>Submit</button>
             </div>
-            )
-          })
-        }
+          );
+        })}
       </div>
     </div>
   );
