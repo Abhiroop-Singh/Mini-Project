5c0e1035cc2e61a90b560c28a96ce1a5e05c4d64
Harsh Dutta Tewari <103374169+Harsh-Tewari@users.noreply.github.com>
2023-05-02 20:01:07 +0530

    Show hide form added

--- client/src/components/GovDashboard/GovDashboard.jsx
+++ client/src/components/GovDashboard/GovDashboard.jsx
@@ -1,10 +1,10 @@
-import React,{useState,useEffect} from "react";
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./GovDashboard.css";
 import {
   Box,
   Container,
-  Tabs, 
+  Tabs,
   Tab,
   TabList,
   TabPanels,
@@ -12,72 +12,70 @@ import {
 } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
-
 const GovDashboard = () => {
+  const [tenderTitle, setTenderTitle] = useState("");
+  const [authority, setAuthority] = useState("");
+  const [referenceNumber, setReferenceNumber] = useState("");
+  const [bidOpeningDate, setBidOpeningDate] = useState("");
+  const [bidClosingDate, setBidClosingDate] = useState("");
+  const [tenderDetails, setTenderDetails] = useState("");
+  const [location, setLocation] = useState("");
+  const [status, setStatus] = useState("");
+  // const [image, setImage] = useState({});
+  const [show, setShow] = useState(false);
 
-const [tenderTitle, setTenderTitle] = useState("");
-const [authority, setAuthority] = useState("");
-const [referenceNumber, setReferenceNumber] = useState("");
-const [bidOpeningDate, setBidOpeningDate] = useState("");
-const [bidClosingDate, setBidClosingDate] = useState("");
-const [tenderDetails, setTenderDetails] = useState("");
-const [location, setLocation] = useState("");
-const [status, setStatus] = useState("");
-const [image,setImage] = useState({});
+  const [active, setActive] = useState([]);
+  const [allotted, setAllotted] = useState([]);
+  const [closed, setClosed] = useState([]);
+  let a = [];
+  let b = [];
+  let c = [];
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    const res = await axios.get("/api/tender/tenderdisplay");
+    const data = res.data.data;
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].status === "active") {
+        a.push(data[i]);
+      } else if (data[i].status === "alloted") {
+        b.push(data[i]);
+      } else {
+        c.push(data[i]);
+      }
+    }
+    setActive(a);
+    setAllotted(b);
+    setClosed(c);
+  };
 
-const [active, setActive] = useState([]);
-const [allotted, setAllotted] = useState([]);
-const [closed, setClosed] = useState([]);
-let a=[];let b=[];let c=[]
-useEffect(() => {
- fetchData();
-}, [])
-const fetchData=async()=>{
-  const res=await axios.get('/api/tender/tenderdisplay');
-  const data = res.data.data;
-  for(let i=0;i<data.length;i++){
-    if(data[i].status==="active"){
-        a.push(data[i])
+  const handleChange = (e) => {
+    if (e.target.name === "tenderTitle") {
+      setTenderTitle(e.target.value);
     }
-    else if(data[i].status==="alloted"){
-      b.push(data[i])
+    if (e.target.name === "authority") {
+      setAuthority(e.target.value);
     }
-    else{
-      c.push(data[i]);
+    if (e.target.name === "referenceNumber") {
+      setReferenceNumber(e.target.value);
     }
-  }
-  setActive(a);
-  setAllotted(b);
-  setClosed(c);
-  
-}
-
-const handleChange=(e)=>{
-  if(e.target.name==="tenderTitle"){
-    setTenderTitle(e.target.value)
-  } 
-  if(e.target.name==="authority"){
-    setAuthority(e.target.value)
-  } 
-  if(e.target.name==="referenceNumber"){
-    setReferenceNumber(e.target.value)
-  } 
-  if(e.target.name==="bidOpeningDate"){
-    setBidOpeningDate(e.target.value)
-  } 
-  if(e.target.name==="bidClosingDate"){
-    setBidClosingDate(e.target.value)
-  } 
-  if(e.target.name==="location"){
-    setLocation(e.target.value)
-  } 
-  if(e.target.name==="status"){
-    setStatus(e.target.value)
-  } 
-  if(e.target.name==="tenderDetails"){
-    setTenderDetails(e.target.value)
-  }
-}
+    if (e.target.name === "bidOpeningDate") {
+      setBidOpeningDate(e.target.value);
+    }
+    if (e.target.name === "bidClosingDate") {
+      setBidClosingDate(e.target.value);
+    }
+    if (e.target.name === "location") {
+      setLocation(e.target.value);
+    }
+    if (e.target.name === "status") {
+      setStatus(e.target.value);
+    }
+    if (e.target.name === "tenderDetails") {
+      setTenderDetails(e.target.value);
+    }
+  };
 
   return (
     <>
@@ -142,43 +140,43 @@ const handleChange=(e)=>{
                         <th>Closing Date</th>
                         <th>Bid Opening Date</th>
                       </tr>
-                      {
-                        active.map((e)=>{
-                          return   <tr>
-                          <td>{e.tenderTitle}</td>
-                          <td>{e.referenceNumber}</td>
-                          <td>{e.bidOpeningDate}</td>
-                          <td>{e.bidClosingDate}</td>
-                        </tr>
-                        })
-                      }
-                    
-                     
-                    </table>
-                  </TabPanel>
-                  <TabPanel>
-                    <table className="gdbtable">
-                      {allotted.map((e)=>{
-                        return  <tr>
-                        <td>{e.tenderTitle}</td>
-                        <td>{e.referenceNumber}</td>
-                        <td>{e.bidOpeningDate}</td>
-                        <td>{e.bidClosingDate}</td>
-                      </tr>
+                      {active.map((e) => {
+                        return (
+                          <tr>
+                            <td>{e.tenderTitle}</td>
+                            <td>{e.referenceNumber}</td>
+                            <td>{e.bidOpeningDate}</td>
+                            <td>{e.bidClosingDate}</td>
+                          </tr>
+                        );
                       })}
-                     
-                     
                     </table>
                   </TabPanel>
                   <TabPanel>
                     <table className="gdbtable">
-                    {closed.map((e)=>{
-                        return  <tr>
-                        <td>{e.tenderTitle}</td>
-                        <td>{e.referenceNumber}</td>
-                        <td>{e.bidOpeningDate}</td>
-                        <td>{e.bidClosingDate}</td>
-                      </tr>
+                      {allotted.map((e) => {
+                        return (
+                          <tr>
+                            <td>{e.tenderTitle}</td>
+                            <td>{e.referenceNumber}</td>
+                            <td>{e.bidOpeningDate}</td>
+                            <td>{e.bidClosingDate}</td>
+                          </tr>
+                        );
+                      })}
+                    </table>
+                  </TabPanel>
+                  <TabPanel>
+                    <table className="gdbtable">
+                      {closed.map((e) => {
+                        return (
+                          <tr>
+                            <td>{e.tenderTitle}</td>
+                            <td>{e.referenceNumber}</td>
+                            <td>{e.bidOpeningDate}</td>
+                            <td>{e.bidClosingDate}</td>
+                          </tr>
+                        );
                       })}
                     </table>
                   </TabPanel>
@@ -187,21 +185,118 @@ const handleChange=(e)=>{
             </Box>
           </Container>
 
-          
-          <form action="/api/tender/uploadTender" method="POST" encType="multipart/form-Data">
-          <input type="text" name="tenderTitle" value={tenderTitle} onChange={handleChange} placeholder="TenderTitle"/> 
-          <input type="text" name="authority" value={authority} onChange={handleChange}placeholder="Authority"/>
-          <input type="text" name="referenceNumber" value={referenceNumber} onChange={handleChange} placeholder="ReferenceNumber"/> 
-          <input type="date" name="bidOpeningDate" value={bidOpeningDate} onChange={handleChange}/> 
-          <input type="date" name="bidClosingDate" value={bidClosingDate} onChange={handleChange}/> 
-          <input type="text" name="tenderDetails" value={tenderDetails} onChange={handleChange}placeholder="Tender Details"/> 
-          <input type="text" name="location" value={location} onChange={handleChange} placeholder="Location"/>
-          <input type="text" name="status" value={status} onChange={handleChange} placeholder="Status"/> 
-          <input type="file" name="image" id="testImage"></input>
-          <input type="submit"></input>
-          </form>
-        </div> 
-      </div> 
+          <button
+            className="showbutton"
+            onClick={(e) => {
+              setShow(!show);
+            }}
+          >
+            Publish a new Tender
+          </button>
+
+          {show && (
+            <form
+              action="/api/tender/uploadTender"
+              method="POST"
+              encType="multipart/form-Data"
+              id="govform"
+            >
+              <div className="govforminput">
+                <h4>Enter Tender Title</h4>
+                <input
+                  type="text"
+                  name="tenderTitle"
+                  value={tenderTitle}
+                  onChange={handleChange}
+                  placeholder="TenderTitle"
+                  className="innergovip"
+                />
+              </div>
+              <div className="govforminput">
+                <h4>Enter Name of the Authority</h4>
+                <input
+                  type="text"
+                  name="authority"
+                  value={authority}
+                  onChange={handleChange}
+                  placeholder="Authority"
+                  className="innergovip"
+                />
+              </div>
+              <div className="govforminput">
+                <h4>Enter a Reference Number</h4>
+                <input
+                  type="text"
+                  name="referenceNumber"
+                  value={referenceNumber}
+                  onChange={handleChange}
+                  placeholder="ReferenceNumber"
+                  className="innergovip"
+                />
+              </div>
+              <div className="govforminput">
+                <h4>Enter Bid-Opening Date</h4>
+                <input
+                  type="date"
+                  name="bidOpeningDate"
+                  value={bidOpeningDate}
+                  onChange={handleChange}
+                  className="innergovip"
+                />
+              </div>
+              <div className="govforminput">
+                <h4>Enter Bid-Closing Date</h4>{" "}
+                <input
+                  type="date"
+                  name="bidClosingDate"
+                  value={bidClosingDate}
+                  onChange={handleChange}
+                  className="innergovip"
+                />
+              </div>
+              <div className="govforminput">
+                <h4>Enter Tender details</h4>
+                <textarea
+                  name="tenderDetails"
+                  value={tenderDetails}
+                  onChange={handleChange}
+                  placeholder="Tender Details"
+                  className="textareagov"
+                />
+              </div>
+              <div className="govforminput">
+                <h4>Enter the Location</h4>
+                <input
+                  type="text"
+                  name="location"
+                  value={location}
+                  onChange={handleChange}
+                  placeholder="Location"
+                  className="innergovip"
+                />
+              </div>
+              <div className="govforminput">
+                <h4>Enter the Status</h4>
+                <input
+                  type="text"
+                  name="status"
+                  value={status}
+                  onChange={handleChange}
+                  placeholder="Status"
+                  className="innergovip"
+                />
+              </div>
+              <div className="govforminput">
+                <h4>Upload a file</h4>
+                <input type="file" name="image" id="testImage"></input>
+              </div>
+              <button type="submit" className="govSubmit">
+                Submit Tender
+              </button>
+            </form>
+          )}
+        </div>
+      </div>
     </>
   );
 };
